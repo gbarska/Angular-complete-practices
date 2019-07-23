df81a45ff3b0c7d2f88ef9b5d820c345d0d3db1c
gbarska <gbarska@yahoo.com>
2019-07-23 00:10:13 -0300

    refactoring to add routing

---
+++ recipes-angular-app/src/app/app-routing.module.ts
@@ -0,0 +1,31 @@
+import { Routes, RouterModule } from '@angular/router';
+import { NgModule } from '@angular/core';
+import { AppComponent } from './app.component';
+import { RecipesComponent } from './recipes/recipes.component';
+import { ShoppingListComponent } from './shopping-list/shopping-list.component';
+import { RecipeEditComponent } from './recipe-edit/recipe-edit.component';
+import { RecipeListComponent } from './recipes/recipe-list/recipe-list.component';
+
+const appRoutes: Routes = [
+    {path: '', component: RecipesComponent},
+    {path: 'shopping', component: ShoppingListComponent},
+    {path: 'recipes', component: RecipesComponent, children: [
+      {path: ':id/edit', component: RecipeEditComponent},
+      {path: '', component: RecipeListComponent}, 
+      {path: 'edit', component: RecipeEditComponent}
+    ]}
+  ]
+
+@NgModule({
+imports: [
+    RouterModule.forRoot(appRoutes)
+  ],
+exports: [
+    RouterModule
+]
+})
+
+
+export class AppRoutingModule { }
+
+
--- recipes-angular-app/src/app/app.module.ts
+++ recipes-angular-app/src/app/app.module.ts
@@ -13,6 +13,8 @@ import { ShoppingListComponent } from './shopping-list/shopping-list.component';
 import { ShoppingEditComponent } from './shopping-list/shopping-edit/shopping-edit.component';
 import { DropdownDirective } from './shared/dropdown.directive';
 import { ShoppingListService } from './shopping-list/shopping-list.service';
+import { AppRoutingModule } from './app-routing.module';
+import { RecipeEditComponent } from './recipe-edit/recipe-edit.component';
 
 @NgModule({
   declarations: [
@@ -24,11 +26,13 @@ import { ShoppingListService } from './shopping-list/shopping-list.service';
     RecipeItemComponent,
     ShoppingListComponent,
     ShoppingEditComponent,
-    DropdownDirective
+    DropdownDirective,
+    RecipeEditComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
+    AppRoutingModule
   ],
   providers: [ShoppingListService],
   bootstrap: [AppComponent]
