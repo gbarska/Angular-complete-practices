7cb58e5ec3fb0e7cd17e7c779cce5b6294e46a5d
Gustavo B Barska <gbarska@yahoo.com>
2019-09-07 18:31:34 -0300

    seting data programmatically

--- forms-td/src/app/app.component.ts
+++ forms-td/src/app/app.component.ts
@@ -8,10 +8,30 @@ import { NgForm } from '@angular/forms';
 })
 export class AppComponent {
   @ViewChild('fo') myForm: NgForm;
-  resposta = "";
+  resposta = "";  
+  genders = ['male','female'];
 
   suggestUserName() {
     const suggestedName = 'Superuser';
+
+    // not the best approach it overrides the all previous data, set the whole form
+    // this.myForm.setValue({
+    //   userData:{
+    //     username: suggestedName,
+    //     email:'',
+       
+    //   },
+    //   gender: 'male',
+    //   secret: 'pet', 
+    //   reply:''
+    // })
+
+    //better approach override only specific fields
+    this.myForm.form.patchValue({
+      userData:{
+        username: suggestedName
+      }
+    })
   }
 
   // onSubmit(form: NgForm){
@@ -20,6 +40,6 @@ export class AppComponent {
 
   // accessing the form with viewchild
   onSubmit(form: NgForm){
-      console.log(this.myForm.controls['email']);
+      console.log(this.myForm);
     }
 }
