3f3b83feb0a418355950995990e78c06fe2393e7
Gustavo B Barska <gbarska@yahoo.com>
2019-09-08 18:49:43 -0300

    handling pure property on pipes and using async pipes

--- pipes-transforming-data/src/app/app.component.ts
+++ pipes-transforming-data/src/app/app.component.ts
@@ -6,6 +6,14 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  filteredStatus = '';
+  
+  appStatus = new Promise((resolve, reject) => {
+    setTimeout(()=>{
+      resolve('stable');
+    },2000)
+  });
+
   servers = [
     {
       instanceType: 'medium',
@@ -38,5 +46,14 @@ export class AppComponent {
       'list-group-item-warning': server.status === 'offline',
       'list-group-item-danger': server.status === 'critical'
     };
+  }
+
+  onAddServer() {
+    this.servers.push({
+      instanceType: 'small',
+      name: 'New Server',
+      status: 'stable',
+      started: new Date(15, 1, 2017)
+    });
   }
 }
