856e6eb3db4d7f97e424e508891e5e71cb0500dd
Gustavo B Barska <gbarska@yahoo.com>
2019-09-09 00:01:42 -0300

    manipulating requests objects

--- http-requests/src/app/auth-interceptor.service.ts
+++ http-requests/src/app/auth-interceptor.service.ts
@@ -5,8 +5,10 @@ export class AuthInterceptorService implements HttpInterceptor{
     intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
         // alternatively we could bypass some requests depending on some criteria
         // if (req.url !== 'endpoint xyz')
+
+        const modifiedRequest = req.clone({headers: req.headers.append('Auth','xyz')})
         console.log('request is on its way');
-        return next.handle(req);
+        return next.handle(modifiedRequest);
     }
 
 }
